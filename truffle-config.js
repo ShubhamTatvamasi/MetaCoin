ebd9d6dedd97821f03bd4accd382317dc5e6df4f
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2019-03-05 13:01:09 +0530

    modified:   truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const mnemonic = "model jar shed wall faint security surge script great glove find book";
+// 0xE32B48a42BFeBAB813209C8D444904C6bb8CDCeB
+// https://rinkeby.etherscan.io/address/0xE32B48a42BFeBAB813209C8D444904C6bb8CDCeB
 
 module.exports = {
   
