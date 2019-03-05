89eaf32ebfde7539c9b6309bfc076135b4f7d923
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2019-03-05 11:55:06 +0530

    new file:   .gitignore

--- truffle-config.js
+++ truffle-config.js
@@ -4,18 +4,24 @@ module.exports = {
   // You can also follow this format for other networks;
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
-  /*
+  
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: "localhost",
+      port: 8545,
       network_id: "*"
     },
-    test: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x52C3a9B0f293CaC8C1bAAbE5B62524A71211a616", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.4"
     }
   }
-  */
 };
